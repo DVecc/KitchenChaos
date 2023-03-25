0b8a0ccd093b30918d4b817a8c351b94251eedd6
Vincent Del Vecchio <21961826+DVecc@users.noreply.github.com>
2023-03-25 02:35:12 -0400

    Add: PlayerAnimator.cs Script
    Add Bool and Getter function in Player.cs to indicate when player is walking
    for use by the animation controller.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
 
   [SerializeField]  private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -33,9 +35,15 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         inputVector.GetType();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
