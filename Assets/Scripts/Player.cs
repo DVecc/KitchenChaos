2469d2985005cf23e8278ce3c8a7a2996612c20b
Vincent Del Vecchio <21961826+DVecc@users.noreply.github.com>
2023-03-29 00:36:37 -0400

    Add: Collision detection to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,12 +15,47 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        
+        float PlayerRadius = .7f;
+        float playerHeight = 2f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDir, moveDistance);
+        
+        if(!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x,0,0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDirX, moveDistance);
 
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDirZ, moveDistance);
+
+                if(canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+        
         isWalking = moveDir != Vector3.zero;
+        
         float rotateSpeed = 10f;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        inputVector.GetType();
+        
     }
 
     public bool IsWalking()
