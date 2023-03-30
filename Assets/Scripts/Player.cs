0012077017c12666e6b510cd17ae8e60117cf333
Vincent Del Vecchio <21961826+DVecc@users.noreply.github.com>
2023-03-30 02:06:08 -0400

    Add: ClearCounter prefab and code for interactions
    Add ClearCounter prefab and script
    Add code to handle interaction with objects in Player script.
    Add layer for counters, for layermask when performing raycast for
    	interactions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,39 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{ 
+{
 
-    [SerializeField]  private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
     private bool isWalking;
-
+    private Vector3 lastInteractDirection;
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        
+
         float PlayerRadius = .7f;
         float playerHeight = 2f;
         float moveDistance = moveSpeed * Time.deltaTime;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDir, moveDistance);
-        
-        if(!canMove)
+
+        if (!canMove)
         {
-            Vector3 moveDirX = new Vector3(moveDir.x,0,0).normalized;
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDirX, moveDistance);
 
             if (canMove)
@@ -35,7 +48,7 @@ public class Player : MonoBehaviour
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDirZ, moveDistance);
 
-                if(canMove)
+                if (canMove)
                 {
                     moveDir = moveDirZ;
                 }
@@ -50,16 +63,34 @@ public class Player : MonoBehaviour
         {
             transform.position += moveDir * moveSpeed * Time.deltaTime;
         }
-        
+
         isWalking = moveDir != Vector3.zero;
-        
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        
     }
 
-    public bool IsWalking()
+    private void HandleInteractions()
     {
-        return isWalking;
+        float interactDistance = 2f;
+
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if(moveDir != Vector3.zero)
+        {
+            lastInteractDirection = moveDir;
+        }
+        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+        else
+        {
+            Debug.Log("-");
+        }
     }
 }
